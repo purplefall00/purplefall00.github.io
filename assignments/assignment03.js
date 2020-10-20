2bee2d9372295e9b70c1227d30ba4d24a92c8314
purplefall00 <71280675+purplefall00@users.noreply.github.com>
2020-10-19 23:32:57 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -6,7 +6,7 @@ elem[1]  = document.getElementById("1");
  console.log(elem.children.length);
 // display the content of the + button, which is the first child of the fifth element
  console.log(elem.children[4].children[0]); 
-
+<button onclick="myFunction()">Click me</button>
 // assign a function to the + button
 elem[1].children[4].children[0].onclick 
   = function(){add1(elem[1]);};
