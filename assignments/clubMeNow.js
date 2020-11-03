dd484a0fb76a1722bcea524f7593531e76d3bbe8
purplefall00 <71280675+purplefall00@users.noreply.github.com>
2020-11-03 01:01:52 -0500

    Update clubMeNow.js

--- assignments/clubMeNow.js
+++ assignments/clubMeNow.js
@@ -1,97 +1,22 @@
- -------------------------------
-Functions in this app:
-
-clubDistanceList.html
-
-  loadClubDistances()
-  appendTableRows()
-  displayClubEntry()
-  displayClubDistanceEntryForm(c)
-  undoLastShot()
-  resetAllClubDistances()
-  displayAbout()
-  
-clubEntry.html
- 
-  addClub()
-  cancelClub()
-  
-clubDistanceEntry.html
-
-  populateStatsTable()
-  appendTapEntryButtons()
-    updateStats()
-
---------------------------------- 
-
-
 // ----- Functions (clubDistanceList.html) -----
+// possible features to add: reset one specific club, add/remove a club,
+// multiple "undo" (undo history), add "confirm" before reset/undo,
+// histogram of club distances,
+// move Bootstrap files to local subdirectory so works if no Internet
+
 
 // initialize "clubs" array
 function loadClubDistances() {
-	let clubs;
 	// if "clubs" array already exists, load it from local storage
 	if (localStorage.getItem("clubs")) {
 		clubs = JSON.parse(localStorage.getItem("clubs"));
 	}
 	// otherwise create new "clubs" array, using resetAllClubs()
 	else {
-		clubs = resetAllClubDistances();
+		resetAllClubDistances();
 		clubs = JSON.parse(localStorage.getItem("clubs"));
 	}
 	return clubs;
-}
-
-// append one row to HTML table for each row in "clubs" array
-function appendTableRows() {
-	// select the HTML table <tbody> element
-	let tblbody = document.getElementById('clubTable').children[0]; 
-	// append one row to HTML table for each row in "clubs" array
-	for (let i = 0; i < clubs.length; i++) {
-		// create an empty row
-		let row = tblbody.insertRow(i+1); // skip first row (column headings)
-		// create an empty cell for each column to appear in HTML table
-		let cell0 = row.insertCell(0); // clubAbbrev
-		let cell1 = row.insertCell(1); // avgDist
-		let cell2 = row.insertCell(2); // minDist
-		let cell3 = row.insertCell(3); // maxDist
-		let cell4 = row.insertCell(4); // numOfShots
-		let cell5 = row.insertCell(5); // ("+" button)
-		let cell6 = row.insertCell(6); // clubName
-		// right align only the cells that need to be right aligned
-		cell0.className = "cmn_hidden"; // clubAbbrev
-		cell1.className = "cmn_alignRight cmn_fullHeight"; // avgDist
-		cell2.className = "cmn_alignRight cmn_hidden"; // minDist
-		cell3.className = "cmn_alignRight cmn_fullHeight"; // maxDist
-		cell4.className = "cmn_alignRight cmn_hidden"; // numOfShots
-		cell5.className = ""; // ("+" button)
-		cell6.className = "cmn_fullHeight";
-		// populate HTML table with data from "clubs" array
-		cell0.innerHTML = clubs[i][1]; // clubAbbrev
-		cell1.innerHTML = Math.round(clubs[i][3]); // avgDist
-		cell2.innerHTML = Math.round(clubs[i][4]); // minDist
-		cell3.innerHTML = Math.round(clubs[i][5]); // maxDist
-		cell4.innerHTML = Math.round(clubs[i][6]); // numOfShots
-		cell5.innerHTML = "<button class='btn btn-success cmn_noPadding cmn_fullHeight' onclick='displayClubDistanceEntryForm(" + i + ");'>&nbsp;&nbsp;+&nbsp;&nbsp;</button>";
-		cell6.innerHTML = clubs[i][2]; // clubName
-		// cell6.innerHTML = clubs[i][2] + ", " + clubs[i][7] + "&deg;"; 
-	}
-}
-
-// navigate to "club ENTRY" screen (enter a new club, not a distance)
-function displayClubEntry() {
-	window.location.href = "clubEntry.html"; 
-}
-
-// navigate to "Distance Entry" screen (from one of the club "+" buttons)
-function displayClubDistanceEntryForm(c) {
-	localStorage.setItem("club", c); // save chosen club
-	window.location.href = "clubDistanceEntry.html"; // redirect to entry form
-}
-
-// replace the current "clubs" array with the previous one
-function undoLastShot() {
-        // your code here !
 }
 
 // create a new (default) "clubs" array
@@ -100,7 +25,7 @@ function resetAllClubDistances() {
 	// columns - 0: sortPosition, 1: clubAbbrev, 2: clubName, 
 	// 3: avgDist, 4: minDist, 5: maxDist, 6: numOfShots, 
 	// 7: loft/degrees, 8: typical/men, 9: typical/women
-	let clubs = [
+	clubs = [
 		[ 199, "Dr",  "Driver",   0, 0, 0, 0, 10.5, 230, 200],
 		[ 300, "3+w", "3+ wood",  0, 0, 0, 0, 13.5, 210, 180],
 		[ 350, "3h",  "3 hybrid", 0, 0, 0, 0, 18.0, 180, 160],
@@ -116,19 +41,68 @@ function resetAllClubDistances() {
 		[1299, "Gw",  "Gap",      0, 0, 0, 0, 51.0,  90,  70],
 		[1399, "Sw",  "Sand",     0, 0, 0, 0, 56.0,  80,  60],
 		[1499, "Lw",  "Lob",      0, 0, 0, 0, 60.0,  60,  40],
-		[1599, "Ptr", "Putter",   0, 0, 0, 0, 60.0,   3,   3],
+		//[1599, "Ptr", "Putter",   0, 0, 0, 0, 60.0,   3,   3],
 	];
 	// store the array in local storage
-	let str = JSON.stringify(clubs);
+	var str = JSON.stringify(clubs);
 	localStorage.setItem("clubs", str);
 	// and refresh screen
 	window.location.href = "clubDistanceList.html"; 
 }
 
+// append one row to HTML table for each row in "clubs" array
+function appendTableRows() {
+	// select the HTML table 
+	var tbl = document.getElementById('clubTable'); 
+	// append one row to HTML table for each row in "clubs" array
+	for (var i = 0; i < clubs.length; i++) {
+		// create an empty row
+		var row = tbl.insertRow(i+1); // skip first row (column headings)
+		// create an empty cell for each column to appear in HTML table
+		var cell0 = row.insertCell(0); // clubAbbrev
+		var cell1 = row.insertCell(1); // avgDist
+		var cell2 = row.insertCell(2); // minDist
+		var cell3 = row.insertCell(3); // maxDist
+		var cell4 = row.insertCell(4); // numOfShots
+		var cell5 = row.insertCell(5); // ("+" button)
+		var cell6 = row.insertCell(6); // clubName
+		// right align only the cells that need to be right aligned
+		cell0.className = "cmn_hidden"; // clubAbbrev
+		cell1.className = "cmn_alignRight cmn_fullHeight"; // avgDist
+		cell2.className = "cmn_alignRight cmn_hidden"; // minDist
+		cell3.className = "cmn_alignRight cmn_fullHeight"; // maxDist
+		cell4.className = "cmn_alignRight cmn_hidden"; // numOfShots
+		cell5.className = ""; // ("+" button)
+		cell6.className = "cmn_fullHeight";
+		// populate HTML table with data from "clubs" array
+		cell0.innerHTML = clubs[i][1]; // clubAbbrev
+		cell1.innerHTML = Math.round(clubs[i][3]); // avgDist
+		cell2.innerHTML = Math.round(clubs[i][4]); // minDist
+		cell3.innerHTML = Math.round(clubs[i][5]); // maxDist
+		cell4.innerHTML = Math.round(clubs[i][6]); // numOfShots
+		cell5.innerHTML = "<button class='btn btn-success cmn_noPadding cmn_fullHeight' onclick='displayclubDistanceEntryForm(" + i + ");'>&nbsp;&nbsp; + &nbsp;&nbsp;</button>";
+		cell6.innerHTML = clubs[i][2]; // clubName
+		// cell6.innerHTML = clubs[i][2] + ", " + clubs[i][7] + "&deg;"; 
+	}
+}
+
+// navigate to "Distance Entry" screen
+function displayclubDistanceEntryForm(c) {
+	localStorage.setItem("club", c); // save chosen club
+	window.location.href = "clubDistanceEntry.html"; // redirect to entry form
+}
+
+// replace the current "clubs" array with the previous one
+function undoLastShot() {
+		// your code here ! 
+	 var oldClubs= localStorage.getItem("clubsUndo");
+	localStorage.setItem("clubs",oldClubs)
+	 window.location.href = "clubDistanceList.html"; 
+}
+
 // navigate to "About" screen
 function displayAbout() {
-	// your code here
-	// window.location.href = "clubAbout.html";
+	alert("ClubMeNow version 1.0.0");
 }
 
 // navigate to "Penalty Info" screen
@@ -137,6 +111,9 @@ function displayPenaltyInfo() {
 }
 
 // ----- Functions (clubDistanceEntry.html) -----
+// features to add: pre-set distance buttons only "realistic" distances,
+// how many yards left/right of target, lie (fairway, rough, sand, trees),
+// add error checking for distances > 400
 
 // put precise numbers (not rounded) in stats HTML table
 function populateStatsTable() {
@@ -150,19 +127,19 @@ function populateStatsTable() {
 // show fast-entry buttons in decrements of 5 yards
 function appendTapEntryButtons() {
 	// select whole cmn_tapEntry div
-	let teDiv = document.getElementById('cmn_tapEntryButtons'); 
+	var teDiv = document.getElementById('cmn_tapEntryButtons'); 
 	// set reasonable range for tapEntry buttons
-	let variation = 30;
-	let avgDistPlusSome = Math.round(clubs[clubRow][3] + variation);
-	let avgDistMinusSome = Math.max(avgDistPlusSome - 2 * variation, 0);
+	var variation = 30;
+	var avgDistPlusSome = Math.round(clubs[clubRow][3] + variation);
+	var avgDistMinusSome = Math.max(avgDistPlusSome - 2 * variation, 0);
 	// if club has average of zero, include many buttons
 	if (0==Math.round(clubs[clubRow][3])) {
 		avgDistPlusSome = 320;
 		avgDistMinusSome = 0;
 	}
 	// append buttons to div in decrements of 5 yards
-	for (let i = avgDistPlusSome; i > avgDistMinusSome; i -= 5) {
-		let btn = document.createElement("span");
+	for (var i = avgDistPlusSome; i > avgDistMinusSome; i -= 5) {
+		var btn = document.createElement("span");
 		btn.innerHTML = "<button class='cmn_noPadding cmn_fullHeight cmn_tapEntry' onclick='updateStats(" + i + ");'>" + i + "</button> ";
 		if(i==Math.round(clubs[clubRow][3])) { 
 			btn.innerHTML = "<button class='cmn_noPadding cmn_fullHeight cmn_tapEntry' onclick='updateStats(" + i + ");'><b>" + i + "</b></button> ";
@@ -181,8 +158,8 @@ function updateStats(shotDistance=0) {
 		shotDistance = parseInt(document.getElementById('clubVal').value);
 	if(parseInt(shotDistance) > 0) {
 		// save current clubs array for "Undo" functionality
-		let str = JSON.stringify(clubs);
-		localStorage.setItem("clubsUndo", str);
+		var oldClubs = JSON.stringify(clubs);
+		localStorage.setItem("clubsUndo", oldClubs);
 		// update average
 		currentAverage = clubs[clubRow][3];
 		currentNumShots = clubs[clubRow][6];
@@ -198,9 +175,21 @@ function updateStats(shotDistance=0) {
 		if (clubs[clubRow][5]==0 
 			|| shotDistance > clubs[clubRow][5]) clubs[clubRow][5] = shotDistance;
 		// save updated stats in local storage
-		str = JSON.stringify(clubs);
+		var str = JSON.stringify(clubs);
 		localStorage.setItem("clubs", str);
 		// return to list screen
 		window.location.href = "clubDistanceList.html"; 
 	}
+}
+
+// navigate to club distance list screen
+function cancelClub() {
+	window.location.href = "clubDistanceList.html"; 
+}
+
+// navigate to club distance list screen
+function displayClubEntry() {
+	if (clubs.length>=14)
+		alert("Warning: you are only allowed to carry 14 clubs in your golf bag in match play competition.");
+	window.location.href = "clubEntry.html"; 
 }
